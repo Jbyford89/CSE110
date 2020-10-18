bb970f2841843392ddd08c1b49f134e303bd0cde
Jbyford89 <66041341+Jbyford89@users.noreply.github.com>
2020-10-17 21:13:24 -0700

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,32 +1,44 @@
-// Name: ...
-// filename: ...
-import java.util.Scanner;
+// Author: James Byford
+// Filename: Scanner Expressions Lab1
+// Specification: Write a program that will take the average of 3 test scores and return what the average grade is.
+// For: CSE110- Lab #1
+// Time Spent: 2.5 hours
+import java.util.Scanner; //import of Scanner expression
+public class Lab1 {
 
-public class Lab1 
-{
-	public static void main(String[] args) 
-	{
-		double test1 = 0;
-		double test2;
-		double test3;
+	public static void main(String[] args) {
+		
+		double test1, test2, test3 = 0;
 		final double NUM_TESTS = 3;
 		
 		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println("Plz input test 1 score:");
+		//This displays test1 input and output
+		
+		System.out.println("Plz input test 1 score:"); // Prompt output
 		test1 = keyboard.nextDouble();
-		System.out.println("Your test 1 is " + test1);
+		System.out.println("Your test 1 is " + test1); //Prompt output
 		
-		System.out.println("Plz input test 2 score:");
+		//This displays test2 input and output
+
+		System.out.println("Plz input test 2 score:"); //Prompt output
 		test2 = keyboard.nextDouble();
-		System.out.println("Your test 2 is " + test2);
+		System.out. println("Your test 2 is " + test2); //Prompt output
 		
-		System.out.println("Plz input test 3 score:");
+		//This displays test3 input and output
+
+		System.out.println("Plz input test 3 score:"); //Prompt output
+		test3 = keyboard.nextDouble();
+		System.out.println("Your test 3 is " + test3); //Prompt output
 		// -->
 		
 		double avg;
-		avg = (test1 + test2) / NUM_TESTS;
-		System.out.println("Your avg is " + avg);
+		avg = (test1 + test2 + test3) / (double) NUM_TESTS; //Cast NUM_TESTS
+		System.out.println("Your average score is "  + avg); //Prompt output
 		
+		
+
 	}
+
 }
+
